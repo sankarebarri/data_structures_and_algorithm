fe6b8e2cb24f204a594ab0edb1b77f8f88908c16
sankarebarri <sankarebarri_dev@yahoo.com>
2024-09-12 12:39:07 +0000

    undirectedPath

---
+++ javascript/undirectedPath.js
@@ -0,0 +1,43 @@
+const undirectedGraph = (edges, nodeA, nodeB) => {
+  const graph = buildGraph(edges);
+  return hasPath(graph, nodeA, nodeB, new Set());
+};
+
+const hasPath = (graph, src, dst, visited) => {
+  if (src === dst) return true;
+  if (visited.has(src)) return false;
+
+  visited.add(src);
+
+  for (let neighbor of graph[src]) {
+    if (hasPath(graph, neighbor, dst, visited) === true) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const buildGraph = (edges) => {
+  const graph = {};
+  for (let edge of edges) {
+    const [a, b] = edge;
+    if (!(a in graph)) graph[a] = [];
+    if (!(b in graph)) graph[b] = [];
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+  return graph;
+};
+
+const edges = [
+  ["i", "j"],
+  ["k", "i"],
+  ["m", "k"],
+  ["k", "l"],
+  ["o", "n"],
+];
+
+console.log(buildGraph(edges));
+const graph = buildGraph(edges);
+console.log(hasPath(graph, "i", "n", new Set()));
+console.log(hasPath(graph, "i", "l", new Set()));
